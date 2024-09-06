51a69b1c25a333cd9898cf3d88fafe21b35ad78a
munirah63 <Munirah.e.Althefeery1@siu.edu>
2024-09-06 18:35:30 -0500

    Implemented addition feature

--- calculator.c
+++ calculator.c
@@ -1,5 +1,13 @@
 #include<stdio.h>
+double add(double x, double y);
 int main()
 {
-    
+    double m=5, n=6, result;
+    result=add(m,n);
+    printf("The result of the addition is: %lf", result);
+
+}
+double add(double x, double y)
+{
+    return x+y;
 }
