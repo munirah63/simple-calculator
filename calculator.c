74b94e0a718f86a3d35063fc720a85da307a10fe
munirah63 <Munirah.e.Althefeery1@siu.edu>
2024-09-06 18:43:01 -0500

    Implemented multiplication feature

--- calculator.c
+++ calculator.c
@@ -1,11 +1,13 @@
 #include<stdio.h>
 double add(double x, double y);
 double sub(double x, double y);
+double mul(double x, double y);
 int main()
 {
     double m=5, n=6;    
     printf("The result of the addition is: %lf", add(m,n));
     printf("\nThe result of the subtraction is: %lf", sub(m,n));
+    printf("\nThe result of the multiplication is: %lf", mul(m,n));
 
 }
 double add(double x, double y)
@@ -16,4 +18,8 @@ double add(double x, double y)
 double sub(double x, double y)
 {
     return x-y;
+}
+double mul(double x, double y)
+{
+    return x*y;
 }
