1937c2081a325c2389df880b562276d06503ef99
munirah63 <Munirah.e.Althefeery1@siu.edu>
2024-09-06 18:40:29 -0500

    Implemented subtraction feature

--- calculator.c
+++ calculator.c
@@ -1,13 +1,19 @@
 #include<stdio.h>
 double add(double x, double y);
+double sub(double x, double y);
 int main()
 {
-    double m=5, n=6, result;
-    result=add(m,n);
-    printf("The result of the addition is: %lf", result);
+    double m=5, n=6;    
+    printf("The result of the addition is: %lf", add(m,n));
+    printf("\nThe result of the subtraction is: %lf", sub(m,n));
 
 }
 double add(double x, double y)
+
 {
     return x+y;
+}
+double sub(double x, double y)
+{
+    return x-y;
 }
